cc0abc77a54b7ba80b013b7cbfc119178158f11a
Shinseojin <02sjin@naver.com>
2025-04-27 12:59:30 +0900

    feat: 랜덤으로 1~9 숫자 중 서로 다른 3개 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,25 @@
 package baseball;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 실행
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computerNumbers = generateRandomNumbers();
+        System.out.println("컴퓨터 숫자: " + computerNumbers); // (테스트용, 나중에 지워야 해)
+    }
+
+    public static List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+
+        while (numbers.size() < 3) {
+            int randomNumber = random.nextInt(9) + 1; // 1~9 숫자 생성
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 }
